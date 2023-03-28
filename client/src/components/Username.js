1bb1698c368680544b28c6b1921f330b480b02a0
HizbullahBhatti <hizbullah.kcsf18@iba-suk.edu.pk>
2023-03-29 01:05:26 +0500

    Username component designed

--- client/src/components/Username.js
+++ client/src/components/Username.js
@@ -1,26 +1,28 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import avatar from '../assets/profile.png'
+import styles from '../styles/Username.module.css'
 
 const Username = () => {
   return (
     <div className="conatiner mx-auto">
       <div className="flex justify-center items-center h-screen">
-        <div>
+        <div className={styles.glass}>
 
           <div className="title flex flex-col items-center">
             <h4 className='text-5xl font-bold'>Hello Again </h4>
             <span className='py-4 text-xl w-2/3 text-center text-gray-500'>
-              Explore More By 
+              Explore More By Connecting with us
             </span>
           </div>
 
           <form action="" className="py-1">
             <div className="profile flex justify-center py-4">
-              <img src="" alt="avatar" />
+              <img src={avatar} alt="avatar" className={styles.profile_img}/>
             </div>
-            <div className="textbox">
-              <input type="text" placeholder='Username'/>
-              <button type="submit">Submit</button>
+            <div className="textbox flex felx-col items-center gap-6">
+              <input className={styles.textbox} type="text" placeholder='Username'/> 
+              <button className={styles.btn} type="submit">Submit</button>
             </div>
             <div className="text-center py-4">
               <span className='text-gray-500'>Not a Member <Link className='text-green-600' to='/register'>Register</Link></span>
