6a282a3c80dbb921aa849f9731bbd1003ecb82c5
HizbullahBhatti <hizbullah.kcsf18@iba-suk.edu.pk>
2023-03-29 00:16:08 +0500

    first commit

--- client/src/components/Username.js
+++ client/src/components/Username.js
@@ -10,7 +10,7 @@ const Username = () => {
           <div className="title flex flex-col items-center">
             <h4 className='text-5xl font-bold'>Hello Again </h4>
             <span className='py-4 text-xl w-2/3 text-center text-gray-500'>
-              Explore More By Connecting With Us
+              Explore More By 
             </span>
           </div>
 
